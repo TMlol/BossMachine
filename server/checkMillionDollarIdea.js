07aff59d3e5ec897eae16d33b731f42bd0f10d75
Tashkyn Murat <tashkynmurat@gmail.com>
2021-07-22 12:33:15 +0600

    Finished idea routes and middleware for ideas

--- server/checkMillionDollarIdea.js
+++ server/checkMillionDollarIdea.js
@@ -1,4 +1,13 @@
-const checkMillionDollarIdea = () => {};
+const checkMillionDollarIdea = (req, res, next) => {
+  const { weeklyRevenue, numWeeks } = req.body;
+  const total = Number(weeklyRevenue) * Number(numWeeks);
+
+  if (total < 1000000 || !weeklyRevenue || !numWeeks || isNaN(total)) {
+    res.status(400).send();
+  } else {
+    next();
+  }
+};
 
 // Leave this exports assignment so that the function can be used elsewhere
 module.exports = checkMillionDollarIdea;
